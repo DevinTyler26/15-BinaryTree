6f67e9dba29644c2b061d37d2496b2748a676497
Devin Cunningham <dtyler.cunningham@gmail.com>
2018-06-29 14:17:55 -0700

    not passing tests but saving

--- src/__test__/binary-tree.test.js
+++ src/__test__/binary-tree.test.js
@@ -3,11 +3,31 @@
 import binaryTree from '../model/binary-tree';
 import { preOderTraversal, postOrderTraversal, inLineTraveral } from '../lib/traversals';
 
-describe('Pre-Order' () => {
-  test('Expect a string of visted nodes as  ', () => {
+describe('Pre-Order', () => {
+  test('Expect a string of visited nodes as  ', () => {
     let str = '';
     preOderTraversal(binaryTree.root, (nodeValue) => {
-      str =+ `${nodeValue}, `;
+      str += `${nodeValue}, `;
+    });
+    expect(str.trim()).toEqual('1, 2, 6, 7, 8, 9, 3, 4, 5,');
+  });
+});
+
+describe('Post-Order', () => {
+  test('Expect a string of visited nodes as  ', () => {
+    let str = '';
+    postOrderTraversal(binaryTree.root, (nodeValue) => {
+      str += `${nodeValue}, `;
+    });
+    expect(str.trim()).toEqual('1, 2, 6, 7, 8, 9, 3, 4, 5,');
+  });
+});
+
+describe('inLine-Order', () => {
+  test('Expect a string of visited nodes as  ', () => {
+    let str = '';
+    inLineTraveral(binaryTree.root, (nodeValue) => {
+      str += `${nodeValue}, `;
     });
     expect(str.trim()).toEqual('1, 2, 6, 7, 8, 9, 3, 4, 5,');
   });
